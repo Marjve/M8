d6820f03bef3669471eb645e5f9ea19004996feb
Mariam Jvaridze <69250388+Marjve@users.noreply.github.com>
2022-08-05 14:52:56 +0400

    contact-start

---
+++ assets/javascript/script.js
@@ -0,0 +1,5 @@
+let person = {firstName:"Mariam", surName: "Jvaridze", age: 24, email:"mariami.jvaridze.1@iliauni.edu.ge", city:"Tbilisi"};
+let personFullName = person.firstName + " " + person.surName;
+console.log(person);
+console.log(personFullName);
+console.log(personFullName + " is " + person.age + " years old and lives in " + person.city );
