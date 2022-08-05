69d17525cb3fc39e04fbb8932392f1bd0b310c9a
Mariam Jvaridze <69250388+Marjve@users.noreply.github.com>
2022-08-05 23:57:13 +0400

    done

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,3 +3,11 @@ let personFullName = person.firstName + " " + person.surName;
 console.log(person);
 console.log(personFullName);
 console.log(personFullName + " is " + person.age + " years old and lives in " + person.city );
+let inputWrapper = document.querySelector(".input-wrapper");
+let contactBtn = document.querySelector(".contact-btn");
+let messageSent = document.querySelector(".contact-sent")
+
+contactBtn.addEventListener("click", function() {
+    inputWrapper.style.display = "none";
+    messageSent.style.display = "block"
+  });
